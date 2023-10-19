98485e04cdfd3d3afe3baebd13f92cec17ae2f8f
Taehyeonn <97taehyeon@gmail.com>
2023-10-20 00:27:10 +0900

    스프링 전환 셋팅

--- src/main/java/hello/core/order/AppConfig.java
+++ src/main/java/hello/core/order/AppConfig.java
@@ -2,29 +2,36 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-public class AppConfig { //애플리케이션 전체를 설정하고 구성한다는 뜻
+@Configuration
+public class AppConfig {
 
-    //역할과 구현 클래스를 한눈에 볼 수 있다
-
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){
-        return new FixDiscountPolicy();
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 
 }
