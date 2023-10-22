076443adc9a55b2ac5ccaeba4b93ab9abec26304
Taehyeon <97taehyun@gmail.com>
2023-10-22 12:33:34 +0900

    Test: 스프링 없는 순수한 싱글톤 테스트 코드 구현

---
+++ src/test/java/hello/core/singleton/SingletonService.java
@@ -0,0 +1,22 @@
+package hello.core.singleton;
+
+public class SingletonService {
+
+    //1 static 영역에 객체를 딱 1개만 생성
+    private static final SingletonService instance = new SingletonService();
+
+    // public으로 열어서 객체 인스턴스가 필요하면 이 static 메서드를 통해서만 조회하도록 허용
+    public static SingletonService getInstance(){
+        return instance;
+    }
+
+    //3 생성자를 private로 선언하여 외부에서 객체 생성 못하게 제한
+    private SingletonService(){
+
+    }
+
+    public void logic(){
+        System.out.println("싱글톤 객체 로직 호출");
+    }
+
+}
